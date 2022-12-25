650101a465d0951802e78f80ed18f2bf80a15e9e
amakar7 <92967392+amakar7@users.noreply.github.com>
2022-12-24 17:05:15 -0800

    Delete Software

--- ESP_32_V2_code/text.py
+++
@@ -1,2 +0,0 @@
-with open("/sd/antonyISaBITCH.txt", "w") as f:
-    f.write("I like to move it move it!\r\n")
--- ESP_32_V2_code/textread.py
+++
@@ -1,3 +0,0 @@
-with open("/sd/antonyISaBITCH.txt", "r") as f:
-    print("Read line from file:")
-    print(f.readline(), end='')
